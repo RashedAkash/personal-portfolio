c0c58e43b9a346a3d2d7b50415431d64c573d9bb
hablu <131282586+hablu@users.noreply.github.com>
2023-12-11 14:50:37 +0600

    my skills section create

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Hero from '@/components/Hero'
+import MySkills from '@/components/MySkills'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <div >
       <Hero />
+      <MySkills />
    </div>
   )
 }
---
+++ src/components/MySkills.jsx
@@ -0,0 +1,141 @@
+"use client"
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import img1 from '../../public/js.png'
+import img2 from '../../public/react.png'
+import img3 from '../../public/nextpng.png'
+import img4 from '../../public/node-js.png'
+import img5 from '../../public/mongodb.png'
+import img6 from '../../public/express.png'
+import img7 from '../../public/html.png'
+import img8 from '../../public/css.png'
+import img9 from '../../public/tailwind.png'
+import img10 from '../../public/mui.png'
+import img11 from '../../public/Firebase.png'
+import img12 from '../../public/figma.png'
+import Image from "next/image";
+import { motion, useScroll } from "framer-motion"
+
+const MySkills = () => {
+  const { scrollYProgress } = useScroll();
+  const settings = {
+      dots: false,
+      arrows: false,
+      infinite: true,
+      slidesToShow: 8,
+      slidesToScroll: 1,
+      autoplay: true,
+      speed: 2000,
+      autoplaySpeed: 2000,
+      cssEase: "linear"
+    };
+  return (
+    <div>
+       <div>
+        <motion.h2 style={{ scaleX: scrollYProgress }}  className=" font-bold inline-block text-5xl pb-3 mb-10 border-b-4 border-cyan-500 text-pink-700">MY Skills</motion.h2>
+        <Slider {...settings}>
+          <div>
+            <Image
+              src={img1}
+              alt="img"
+              width={80}
+              height={80}
+            />
+          </div>
+          <div>
+            <Image
+              src={img2}
+              alt="img"
+              width={80}
+              height={80}
+            />
+          </div>
+          <div>
+            <Image
+              src={img3}
+              alt="img"
+              width={80}
+              height={80}
+            />
+          </div>
+          <div>
+            <Image
+              src={img4}
+              alt="img"
+              width={80}
+              height={80}
+            />
+          </div>
+          <div>
+            <Image
+              src={img5}
+              alt="img"
+              width={80}
+              height={80}
+            />
+          </div>
+          <div>
+            <Image
+              src={img6}
+              alt="img"
+              width={80}
+              height={80}
+            />
+          </div>
+          <div>
+            <Image
+              src={img7}
+              alt="img"
+              width={80}
+              height={80}
+            />
+          </div>
+          <div>
+            <Image
+              src={img8}
+              alt="img"
+              width={80}
+              height={80}
+            />
+          </div>
+          <div>
+            <Image
+              src={img9}
+              alt="img"
+              width={80}
+              height={80}
+            />
+          </div>
+          <div>
+            <Image
+              src={img10}
+              alt="img"
+              width={80}
+              height={80}
+            />
+          </div>
+          <div>
+            <Image
+              src={img11}
+              alt="img"
+              width={80}
+              height={80}
+            />
+          </div>
+          <div>
+            <Image
+              src={img12}
+              alt="img"
+              width={80}
+              height={80}
+            />
+          </div>
+          
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
+export default MySkills;
